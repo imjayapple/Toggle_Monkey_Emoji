619bef81a679c2ff1217e89b981e619d741fd577
imjayapple <thisisfakeemail@gmai.com>
2022-11-29 19:26:55 -0800

    Debugging: added semi-colons to variable lines, and removed a period from closedFace so that it would respond to first click

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const closedFace = document.querySelector('.closed')
-const openFace = document.querySelector('.open')
+const closedFace = document.querySelector('.closed');
+const openFace = document.querySelector('.open');
 
 // Add event listener
-closed.Face.addEventListener('click', () => {
+closedFace.addEventListener('click', () => {
     if (openFace.classList.contains('open')) {
         openFace.classList.add('active');
         closedFace.classList.remove('active');
